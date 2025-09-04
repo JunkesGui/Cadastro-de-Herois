c0f8ddf355898058b711377e96313dd56f7b077a
JunkesGui <guilherme.junkes1@gmail.com>
2025-09-04 05:00:48 -0300

    Feat #2 Criado uma entidade HeroiModel

--- src/main/java/com/JunkesGui/CadastroDeHerois/HeroiController.java
+++ src/main/java/com/JunkesGui/CadastroDeHerois/HeroiController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class HeroiController {
 
     @GetMapping("/boasvindas")
     public String BemVindo() {
