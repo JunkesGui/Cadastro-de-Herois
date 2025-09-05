b2b2fcda8a2d63b9d37268c166b14969f1dd8b3d
JunkesGui <guilherme.junkes1@gmail.com>
2025-09-05 04:41:36 -0300

    Feat Adciona os arquivos HeroiRepository e MissaoRepository extendendo o JPA para abstracao

---
+++ src/main/java/com/JunkesGui/CadastroDeHerois/Herois/HeroiRepository.java
@@ -0,0 +1,6 @@
+package com.JunkesGui.CadastroDeHerois.Herois;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HeroiRepository extends JpaRepository<HeroiModel, Long> {
+}
---
+++ src/main/java/com/JunkesGui/CadastroDeHerois/Missoes/MissaoRepository.java
@@ -0,0 +1,6 @@
+package com.JunkesGui.CadastroDeHerois.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissaoRepository extends JpaRepository<MissaoModel, Long> {
+}
