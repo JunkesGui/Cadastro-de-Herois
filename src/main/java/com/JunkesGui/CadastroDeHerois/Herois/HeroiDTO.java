ae4d876d1b5e8aa68b4d4abd3747174537333e1d
JunkesGui <guilherme.junkes1@gmail.com>
2025-09-07 04:35:24 -0300

    Chore inclui o DTO do model e mapeia a entidade pelo Mapper

--- src/main/java/com/JunkesGui/CadastroDeHerois/Herois/HeroiDTO.java
+++ src/main/java/com/JunkesGui/CadastroDeHerois/Herois/HeroiDTO.java
@@ -1,4 +1,19 @@
 package com.JunkesGui.CadastroDeHerois.Herois;
 
+import com.JunkesGui.CadastroDeHerois.Missoes.MissaoModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class HeroiDTO {
+
+    private Long id;
+    private String nome;
+    private String classe;
+    private short rank;
+    private String equipamento;
+    private MissaoModel missao;
 }
--- src/main/java/com/JunkesGui/CadastroDeHerois/Herois/HeroiMapper.java
+++ src/main/java/com/JunkesGui/CadastroDeHerois/Herois/HeroiMapper.java
@@ -1,4 +1,34 @@
 package com.JunkesGui.CadastroDeHerois.Herois;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class HeroiMapper {
+
+    public HeroiModel map(HeroiDTO heroiDTO) {
+        HeroiModel heroiModel = new HeroiModel();
+
+        heroiModel.setId(heroiDTO.getId());
+        heroiModel.setNome(heroiDTO.getNome());
+        heroiModel.setClasse(heroiDTO.getClasse());
+        heroiModel.setRank(heroiDTO.getRank());
+        heroiModel.setEquipamento(heroiDTO.getEquipamento());
+        heroiModel.setMissao(heroiDTO.getMissao());
+
+        return heroiModel;
+    }
+
+    public HeroiDTO map(HeroiModel heroiModel) {
+        HeroiDTO heroiDTO = new HeroiDTO();
+
+        heroiDTO.setId(heroiModel.getId());
+        heroiDTO.setNome(heroiModel.getNome());
+        heroiDTO.setClasse(heroiModel.getClasse());
+        heroiDTO.setRank(heroiModel.getRank());
+        heroiDTO.setEquipamento(heroiModel.getEquipamento());
+        heroiDTO.setMissao(heroiModel.getMissao());
+
+        return heroiDTO;
+    }
+
 }
