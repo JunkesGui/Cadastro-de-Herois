bcc591045ded40da7f6d11c63c080ef3fcd3f89a
JunkesGui <guilherme.junkes1@gmail.com>
2025-09-07 08:01:31 -0300

    Docs documenta o HeroiController via Swagger

--- src/main/java/com/JunkesGui/CadastroDeHerois/Herois/HeroiController.java
+++ src/main/java/com/JunkesGui/CadastroDeHerois/Herois/HeroiController.java
@@ -1,5 +1,8 @@
 package com.JunkesGui.CadastroDeHerois.Herois;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,16 +17,28 @@ public class HeroiController {
         this.heroiService = heroiService;
     }
 
-//    Adicionar Heroi
     @PostMapping("/adicionar")
+    @Operation(summary = "Criar Heroi",
+            description = "Rota para criacao de herois novos na tabela TB_CADASTRO")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",
+                    description = "Heroi criado com sucesso!\n ID: {id} Nome: {nome}")
+    })
     public ResponseEntity<String> criarHeroi(@RequestBody HeroiDTO heroi){
         HeroiDTO heroiDTO = heroiService.criarHeroi(heroi);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Heroi criado com sucesso!\n " +
-                "ID: " + heroiDTO.getId() + " Nome: " + heroiDTO.getNome());
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Heroi criado com sucesso!\n" + "ID: " + heroiDTO.getId() + " Nome: " + heroiDTO.getNome());
     }
 
-//    Mostrar Heroi por ID
     @GetMapping("/todos/{id}")
+    @Operation(summary = "Buscar Heroi por ID",
+            description = "Rota para busca de heroi por um ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "{heroi JSON}"),
+            @ApiResponse(responseCode = "404",
+                    description = "Id {id} nao encontrado!")
+    })
     public ResponseEntity<?> mostrarHeroiPorID(@PathVariable long id){
         if (heroiService.mostrarHeroiPorID(id) != null) {
             HeroiDTO heroi = heroiService.mostrarHeroiPorID(id);
@@ -34,19 +49,31 @@ public class HeroiController {
         }
 }
 
-//    Mostrar lista de Herois
     @GetMapping("/todos")
+    @Operation(summary = "Mostra a lista completa de Herois",
+            description = "Rota para mostrar a tabela completa TB_CADASTRO")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "{TB_CADASTRO JSON}")
+    })
     public ResponseEntity<List<HeroiDTO>> mostrarListaHerois(){
         List<HeroiDTO> listaHerois = heroiService.mostrarListaHerois();
         return ResponseEntity.status(HttpStatus.OK).body(listaHerois);
     }
 
-//    Alterar dados de Heroi
     @PutMapping("/alterarid/{id}")
+    @Operation(summary = "Altera dados de um Heroi por ID",
+            description = "Rota que recebe um ID e um JSON para alterar um Heroi cadastrado na tabela TB_CADASTRO")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Heroi de  ID {id} alterado com sucesso!"),
+            @ApiResponse(responseCode = "404",
+                    description = "Id {id} nao encontrado!")
+    })
     public ResponseEntity<String> alterarHeroiID(@PathVariable long id, @RequestBody HeroiDTO heroiUpdated){
         if (heroiService.mostrarHeroiPorID(id) != null) {
             heroiService.alterarHeroiID(id, heroiUpdated);
-            return ResponseEntity.ok("Heroi de ID "+id+"alterado com sucesso!\n");
+            return ResponseEntity.ok("Heroi de ID "+id+" alterado com sucesso!\n");
         }else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body("Id "+id+" nao encontrado!");
@@ -55,6 +82,14 @@ public class HeroiController {
 
 //    Deletar Heroi
     @DeleteMapping("/apagar/{id}")
+    @Operation(summary = "Deleta um Heroi por ID",
+            description = "Rota para deletar um heroi da tabela TB_CADASTRO por ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Heroi de id {id} deletado com sucesso!"),
+            @ApiResponse(responseCode = "404",
+                    description = "Id {id} nao encontrado!")
+    })
     public ResponseEntity<String> apagarHeroiID(@PathVariable long id){
        if (heroiService.mostrarHeroiPorID(id) != null) {
            String heroiName = heroiService.mostrarHeroiPorID(id).getNome();
@@ -65,7 +100,6 @@ public class HeroiController {
            return ResponseEntity.status(HttpStatus.NOT_FOUND)
                    .body("Id "+id+" nao encontrado!");
        }
-
     }
 
 }
